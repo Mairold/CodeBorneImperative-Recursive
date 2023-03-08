2a358eae971ff99c4a1ab4fe4e6b1c565a1d32b7
vali-it <asaas>
2023-03-08 13:47:36 +0200

    Fibbonacci recursive method refactored.

--- src/Fibbonacci.java
+++ src/Fibbonacci.java
@@ -2,24 +2,7 @@ import java.util.ArrayList;
 
 public class Fibbonacci {
 
-    public int returnFibonacci(int n) {
-        if (n == 1) {
-            return 0;
-        } else if (n == 2) {
-            return 1;
-        } else {
-            ArrayList<Integer> fibSec = new ArrayList<>();
-            fibSec.add(0);
-            fibSec.add(1);
-            addNumbersToFibSec(n,fibSec);
-            return fibSec.get(n - 1);
-        }
-    }
-
-    private void addNumbersToFibSec (int n, ArrayList<Integer> fibSec) {
-        if (n >= fibSec.size()) {
-            fibSec.add(fibSec.get(fibSec.size() - 1) + fibSec.get(fibSec.size() - 2));
-            addNumbersToFibSec(n,fibSec);
-        }
+    public int fibbonacciNumberAtPosition(int n) {
+        return n == 1 ? 0 : n == 2 ? 1 : fibbonacciNumberAtPosition(n-1) + fibbonacciNumberAtPosition(n-2);
     }
 }
--- test/FibbonacciTest.java
+++ test/FibbonacciTest.java
@@ -8,21 +8,21 @@ public class FibbonacciTest {
 
     @Test
     void calculate_1() {
-        assertEquals(0, fibbonacci.returnFibonacci(1));
+        assertEquals(0, fibbonacci.fibbonacciNumberAtPosition(1));
     }
 
     @Test
     void calculate_2() {
-        assertEquals(1, fibbonacci.returnFibonacci(2));
+        assertEquals(1, fibbonacci.fibbonacciNumberAtPosition(2));
     }
 
     @Test
     void calculate_10() {
-        assertEquals(34, fibbonacci.returnFibonacci(10));
+        assertEquals(34, fibbonacci.fibbonacciNumberAtPosition(10));
     }
 
     @Test
     void calculate_20() {
-        assertEquals(4181, fibbonacci.returnFibonacci(20));
+        assertEquals(4181, fibbonacci.fibbonacciNumberAtPosition(20));
     }
 }
