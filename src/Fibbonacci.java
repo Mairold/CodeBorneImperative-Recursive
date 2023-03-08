6c5de28a3fdfac41ddbfd76652ab53a91bc55ecd
vali-it <asaas>
2023-03-08 12:13:53 +0200

    Fibbonacci case 2,10,20 added. Imperative method done.

--- src/Fibbonacci.java
+++ src/Fibbonacci.java
@@ -1,6 +1,20 @@
+import java.util.ArrayList;
+
 public class Fibbonacci {
 
     public int returnFibonacci(int n) {
-        return 0;
+        if (n == 1) {
+            return 0;
+        } else if (n == 2) {
+            return 1;
+        } else {
+            ArrayList<Integer> fibSec = new ArrayList<>();
+            fibSec.add(0);
+            fibSec.add(1);
+            while (n >= fibSec.size()) {
+                fibSec.add(fibSec.get(fibSec.size() - 1) + fibSec.get(fibSec.size() - 2));
+            }
+            return fibSec.get(n - 1);
+        }
     }
 }
--- test/FibbonacciTest.java
+++ test/FibbonacciTest.java
@@ -5,8 +5,24 @@ import static org.junit.jupiter.api.Assertions.*;
 public class FibbonacciTest {
 
     private final Fibbonacci fibbonacci = new Fibbonacci();
+
     @Test
-    void calculate() {
+    void calculate_1() {
         assertEquals(0, fibbonacci.returnFibonacci(1));
+    }
+
+    @Test
+    void calculate_2() {
+        assertEquals(1, fibbonacci.returnFibonacci(2));
+    }
+
+    @Test
+    void calculate_10() {
+        assertEquals(34, fibbonacci.returnFibonacci(10));
+    }
+
+    @Test
+    void calculate_20() {
+        assertEquals(4181, fibbonacci.returnFibonacci(20));
     }
 }
